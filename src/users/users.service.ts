0b56cc990a670e0f401beb80973935955603c5c1
Usman Afzal <109211422+usmannafzal@users.noreply.github.com>
2025-01-19 17:09:18 +0500

    Changes made to the script

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,11 +6,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  saltOrRounds: number;
-
-  constructor(@InjectRepository(User) private repo: Repository<User>) {
-    this.saltOrRounds = 10;
-  }
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(data: CreateUserDto) {
     const user = new User();
