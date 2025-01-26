7dc72d39c9afa9d2d20c4f2dbe37c0f8b2b39702
Usman Afzal <109211422+usmannafzal@users.noreply.github.com>
2025-01-26 18:16:51 +0500

    Create API response formatted

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,27 +3,48 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-  create(data: CreateUserDto) {
-    const user = new User();
-    user.name = data.name;
-    user.userName = data.username;
-    user.email = data.email;
-    user.userStreet = data.address.street;
-    user.userSuite = data.address.suite;
-    user.userCity = data.address.city;
-    user.userZipcode = data.address.zipcode;
-    user.companyName = data.company.name;
-    user.companyCatchPhrase = data.company.catchPhrase;
-    user.companyBs = data.company.bs;
-    user.userLat = data.address.geo.lat;
-    user.userLng = data.address.geo.lng;
-    user.phone = data.phone;
-    user.website = data.website;
-    return this.repo.save(user);
+  async create(data: CreateUserDto) {
+    const userEntity = plainToInstance(User, {
+      ...data,
+      userName: data.username,
+      userStreet: data.address.street,
+      userSuite: data.address.suite,
+      userCity: data.address.city,
+      userZipcode: data.address.zipcode,
+      userLat: data.address.geo.lat,
+      userLng: data.address.geo.lng,
+      companyName: data.company.name,
+      companyCatchPhrase: data.company.catchPhrase,
+      companyBs: data.company.bs,
+    });
+
+    const savedUser = await this.repo.save(userEntity);
+
+    return plainToInstance(CreateUserDto, {
+      name: savedUser.name,
+      username: savedUser.userName,
+      email: savedUser.email,
+      address: {
+        street: savedUser.userStreet,
+        suite: savedUser.userSuite,
+        city: savedUser.userCity,
+        zipcode: savedUser.userZipcode,
+        geo: {
+          lat: savedUser.userLat,
+          lng: savedUser.userLng,
+        },
+      },
+      company: {
+        name: savedUser.companyName,
+        catchPhrase: savedUser.companyCatchPhrase,
+        bs: savedUser.companyBs,
+      },
+    });
   }
 }
